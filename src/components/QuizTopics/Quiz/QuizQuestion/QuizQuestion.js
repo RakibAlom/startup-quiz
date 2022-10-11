a88fa42e24073fdd1a54fee366b0af0b2e684b29
RakibAlom <rakibalom17@gmail.com>
2022-10-11 15:26:16 +0600

    question design

--- src/components/QuizTopics/Quiz/QuizQuestion/QuizQuestion.js
+++ src/components/QuizTopics/Quiz/QuizQuestion/QuizQuestion.js
@@ -1,13 +1,29 @@
+import { faSquare } from '@fortawesome/free-regular-svg-icons';
+import { faEye } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
+import './QuizQuestion.css'
 
 const QuizQuestion = ({ question }) => {
   return (
     <>
-      <div className="shadow p-2 p-md-4 p-lg-5 mb-4">
+      <div className="shadow p-2 p-md-4 p-lg-5 mb-4 question">
         <h2 className='text-center'>{question.question}</h2>
-        {
-          question.options.map((option, index) => <li key={index}>{option}</li>)
-        }
+        <div className="quiz-options">
+          {
+            question.options.map((option, index) =>
+              <div className="option d-flex px-3 bg-light rounded gap-3 align-items-center">
+                <span className='fs-3'><FontAwesomeIcon icon={faSquare} /></span>
+                <p className='fs-5 m-0'>{option}</p>
+              </div>
+            )
+          }
+        </div>
+
+        <span className='view-icon fs-3'>
+          <FontAwesomeIcon icon={faEye} />
+        </span>
+
       </div>
     </>
   );
