07cf39a615d76e5aa3b8e8c341ecf38d86fd985c
RakibAlom <rakibalom17@gmail.com>
2022-10-12 02:46:39 +0600

    quiz select option

--- src/components/QuizTopics/Quiz/QuizQuestion/QuizQuestion.js
+++ src/components/QuizTopics/Quiz/QuizQuestion/QuizQuestion.js
@@ -1,26 +1,40 @@
-import { faSquare } from '@fortawesome/free-regular-svg-icons';
+import { faSquare, faSquareCheck } from '@fortawesome/free-regular-svg-icons';
 import { faEye } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React from 'react';
+import React, { useState } from 'react';
 import './QuizQuestion.css'
 
 const QuizQuestion = ({ question }) => {
+  const [selected, setSelected] = useState("");
+  // const [score, setScore] = useState(0);
+  const [correctAnswer, setCorrectAnswer] = useState(false);
+
+  const handleSelected = (question, option) => {
+    setSelected(option)
+  }
+
+  const showCorrectAnswer = () => {
+    setCorrectAnswer(!correctAnswer);
+  }
+
   return (
     <>
       <div className="shadow p-2 p-md-4 p-lg-5 mb-4 question">
-        <h2 className='text-center'>{question.question}</h2>
+
+        <h2 className='text-center px-3' dangerouslySetInnerHTML={{ __html: question.question }}></h2>
+
         <div className="quiz-options">
           {
             question.options.map((option, index) =>
-              <div className="option d-flex px-3 bg-light rounded gap-3 align-items-center">
-                <span className='fs-3'><FontAwesomeIcon icon={faSquare} /></span>
+              <div onClick={() => handleSelected(question, option)} className={`option d-flex px-3 py-1 ${selected === option ? "bg-success text-white" : "bg-light"} rounded gap-3 align-items-center`} key={index}>
+                <span className='fs-3'><FontAwesomeIcon icon={selected === option ? faSquareCheck : faSquare} /></span>
                 <p className='fs-5 m-0'>{option}</p>
               </div>
             )
           }
         </div>
 
-        <span className='view-icon fs-3'>
+        <span className='view-icon fs-3' onClick={() => showCorrectAnswer()}>
           <FontAwesomeIcon icon={faEye} />
         </span>
 
